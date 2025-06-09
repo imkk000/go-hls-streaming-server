3bf927b714e0cb5fa4e23de97bd73a4e3884b8b3
NB <nattakit.boonyang@gmail.com>
2025-06-09 23:38:50 +0700

    Add base path argument

--- script.fish
+++ script.fish
@@ -1,21 +1,24 @@
 #!/usr/bin/env fish
 
-set -l path $argv[1]
-if test -z $path
-    echo "Usage: script.fish <path_to_video>"
+if test -z $argv[1]
+    echo "Usage: script.fish <path_to_segments> <path_to_video> <path_to_video> ..."
     exit 1
 end
 
-set -l filename (path basename $path)
-set -l output (string trim --right --chars=(path extension $filename) $filename)
+set base_path $argv[1]
 
-cd segments
-mkdir $output
-cd $output
+for path in $argv[2..-1]
+  set filename (path basename $path)
+  set output $base_path/(string trim --right --chars=(path extension $filename) $filename)
 
-ffmpeg -i $path -map 0:v -map 0:a -codec: copy \
-  -start_number 0 -hls_time 10 -hls_list_size 0 \
-  -hls_segment_filename "segment_%03d.ts" -f hls playlist.m3u8
-ffmpeg -i $path -map 0:s -c:s webvtt subtitle.vtt
-ffmpeg -i segment_005.ts -frames:v 1 -update 1 thumbnail.png
-ffmpeg -i $path -map 0:v:1 -update 1 cover.jpg
+  mkdir -p $output
+
+  ffmpeg -i $path -map 0:v -map 0:a -codec: copy \
+    -start_number 0 -hls_time 10 -hls_list_size 0 \
+    -hls_segment_filename $output/"segment_%03d.ts" -f hls $output/playlist.m3u8
+  ffmpeg -i $path -map 0:s -c:s webvtt $output/subtitle.vtt
+  ffmpeg -i segment_005.ts -frames:v 1 -update 1 $output/thumbnail.png
+  ffmpeg -i $path -map 0:v:1 -update 1 $output/cover.jpg
+  #
+  # cd ..
+end
