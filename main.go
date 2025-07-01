850534927780be755e344b05f03a4e503916b029
NB <nattakit.boonyang@gmail.com>
2025-07-02 00:15:13 +0700

    Update server

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 )
+
+const segmentsDir = "segments"
 
 type Template struct {
 	templates *template.Template
@@ -22,19 +25,19 @@ func (t *Template) Render(w io.Writer, name string, data any, c echo.Context) er
 
 func main() {
 	e := echo.New()
+	e.HideBanner = true
 	e.Use(
 		middleware.RequestID(),
 		middleware.Recover(),
 		middleware.Logger(),
 	)
 	e.Static("js", "public/js")
-	e.Static("", "segments")
+	e.Static("", segmentsDir)
 	e.Renderer = &Template{
 		templates: template.Must(template.ParseGlob("public/template/*.html")),
 	}
-	e.GET("/", func(c echo.Context) error {
-		path := c.QueryParam("path")
-		dirs, err := os.ReadDir("segments")
+	e.GET("/playlist", func(c echo.Context) error {
+		dirs, err := os.ReadDir(segmentsDir)
 		if err != nil {
 			return err
 		}
@@ -42,21 +45,43 @@ func main() {
 		for i := range dirs {
 			paths[i] = dirs[i].Name()
 		}
+		return c.Render(http.StatusOK, "list.html", echo.Map{
+			"Dirs": paths,
+		})
+	})
+	e.GET("/", func(c echo.Context) error {
+		path := c.QueryParam("path")
+		if path == "" {
+			return c.Redirect(http.StatusMovedPermanently, "/playlist")
+		}
+
+		var subtitles []Subtitle
+		if path != "" {
+			filename := filepath.Join(path, "subtitle.vtt")
+			subtitleFilename := filepath.Join(segmentsDir, filename)
+			if _, err := os.Stat(subtitleFilename); err == nil {
+				subtitles = []Subtitle{
+					{
+						Src:       filename,
+						Lang:      "en",
+						Label:     "English",
+						IsDefault: true,
+					},
+				}
+			}
+		}
+
 		return c.Render(http.StatusOK, "index.html", Render{
-			Dirs: paths,
-			Path: fmt.Sprintf("%s/playlist.m3u8", path),
-			Subtitles: []Subtitle{
-				{
-					Src:       fmt.Sprintf("%s/subtitle.vtt", path),
-					Lang:      "en",
-					Label:     "English",
-					IsDefault: true,
-				},
-			},
+			Path:      fmt.Sprintf("%s/playlist.m3u8", path),
+			Subtitles: subtitles,
 		})
 	})
 
-	addr := "127.0.0.1:54321"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "54321"
+	}
+	addr := fmt.Sprintf("127.0.0.1:%s", port)
 	log.Info().Msgf("start server %s", addr)
 	if err := e.StartTLS(addr, "cert.pem", "key.pem"); err != nil {
 		log.Fatal().Err(err).Msg("start server")
--- script.fish
+++ script.fish
@@ -8,17 +8,14 @@ end
 set base_path $argv[1]
 
 for path in $argv[2..-1]
-  set filename (path basename $path)
-  set output $base_path/(string trim --right --chars=(path extension $filename) $filename)
+    set filename (path basename $path)
+    set output $base_path/(string trim --right --chars=(path extension $filename) $filename)
 
-  mkdir -p $output
+    mkdir -p $output
 
-  ffmpeg -i $path -map 0:v -map 0:a -codec: copy \
-    -start_number 0 -hls_time 10 -hls_list_size 0 \
-    -hls_segment_filename $output/"segment_%03d.ts" -f hls $output/playlist.m3u8
-  ffmpeg -i $path -map 0:s -c:s webvtt $output/subtitle.vtt
-  ffmpeg -i segment_005.ts -frames:v 1 -update 1 $output/thumbnail.png
-  ffmpeg -i $path -map 0:v:1 -update 1 $output/cover.jpg
-  #
-  # cd ..
+    ffmpeg -i $path -map 0:v -map 0:a -codec: copy \
+        -start_number 0 -hls_time 10 -hls_list_size 0 \
+        -hls_segment_filename $output/"segment_%03d.ts" -f hls $output/playlist.m3u8
+    ffmpeg -i $path -map 0:s -c:s webvtt $output/subtitle.vtt
+    ffmpeg -i $path -map 0:v:1 -update 1 $output/cover.jpg
 end
