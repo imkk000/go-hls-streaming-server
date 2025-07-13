6df82916376f0a390c49210680210676f8dcb19f
NB <nattakit.boonyang@gmail.com>
2025-07-13 16:56:30 +0700

    Support series

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/subtle"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -24,36 +28,67 @@ func (t *Template) Render(w io.Writer, name string, data any, c echo.Context) er
 }
 
 func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal().Err(err).Msg("get current working directory")
+	}
+	segmentPath := filepath.Join(wd, segmentsDir)
+
 	e := echo.New()
 	e.HideBanner = true
 	e.Use(
 		middleware.RequestID(),
 		middleware.Recover(),
 		middleware.Logger(),
-		// middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		// 	if subtle.ConstantTimeCompare([]byte(username), []byte("kk")) == 1 &&
-		// 		subtle.ConstantTimeCompare([]byte(password), []byte("CiZei8k*8q-A7nPBWDCNy6wp_.!8qQbCqJ4WQ2u9MuEXTEADwg7yF_!.xyCQZsaC")) == 1 {
-		// 		return true, nil
-		// 	}
-		// 	return false, nil
-		// }),
+		middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+			if subtle.ConstantTimeCompare([]byte(username), []byte("kk")) == 1 &&
+				subtle.ConstantTimeCompare([]byte(password), []byte("CiZei8k*8q-A7nPBWDCNy6wp_.!8qQbCqJ4WQ2u9MuEXTEADwg7yF_!.xyCQZsaC")) == 1 {
+				return true, nil
+			}
+			return false, nil
+		}),
 	)
 	e.Static("js", "public/js")
+	e.Static("css", "public/css")
+	e.Static("fonts", "public/fonts")
 	e.Static("", segmentsDir)
 	e.Renderer = &Template{
 		templates: template.Must(template.ParseGlob("public/template/*.html")),
 	}
 	e.GET("/playlist", func(c echo.Context) error {
-		dirs, err := os.ReadDir(segmentsDir)
+		series := c.QueryParam("s")
+		if series != "" {
+			path, err := filepath.Abs(series)
+			if err != nil {
+				return err
+			}
+			if !strings.HasPrefix(path, wd) {
+				return echo.NewHTTPError(http.StatusForbidden, "Access denied")
+			}
+		}
+		path := filepath.Join(segmentPath, series)
+		dirs, err := os.ReadDir(path)
 		if err != nil {
 			return err
 		}
-		paths := make([]string, len(dirs))
-		for i := range dirs {
-			paths[i] = dirs[i].Name()
+		list := make([]List, 0, len(dirs))
+		for _, dir := range dirs {
+			if !dir.IsDir() {
+				continue
+			}
+			pathName := filepath.Join(series, dir.Name())
+			item := List{
+				Dir: dir.Name(),
+				URL: fmt.Sprintf("/?path=%s", pathName),
+			}
+			next := filepath.Join(path, dir.Name(), "playlist.m3u8")
+			if _, err := os.Stat(next); err != nil {
+				item.URL = fmt.Sprintf("/playlist?s=%s", pathName)
+			}
+			list = append(list, item)
 		}
 		return c.Render(http.StatusOK, "list.html", echo.Map{
-			"Dirs": paths,
+			"Dirs": slices.Clip(list),
 		})
 	})
 	e.GET("js/hls.render.js", func(c echo.Context) error {
@@ -64,7 +99,6 @@ func main() {
 		c.Response().Header().Set("Content-Type", "text/javascript; charset=utf-8")
 		return c.String(http.StatusOK, content)
 	})
-
 	e.GET("/", func(c echo.Context) error {
 		path := c.QueryParam("path")
 		if path == "" {
@@ -88,24 +122,51 @@ func main() {
 }
 
 const hlsScriptTemplate = `
-const video = document.getElementById('player');
-const player = new Plyr(video, {captions: {active: true, update: true}});
+const video = document.getElementById("player");
+const player = new Plyr(video, {
+  controls: [
+    "play-large",
+    "restart",
+    "rewind",
+    "play",
+    "fast-forward",
+    "progress",
+    "current-time",
+    "duration",
+    "mute",
+    "volume",
+    "captions",
+    "settings",
+    "fullscreen",
+  ],
+  captions: {
+    active: true,
+    update: true,
+  },
+});
 const hls = new Hls();
-hls.loadSource('/%s/playlist.m3u8?v=%d');
+hls.loadSource("/%s/playlist.m3u8?v=%d");
 hls.attachMedia(video);
 hls.on(Hls.Events.MANIFEST_LOADED, () => {
-	video.appendChild(Object.assign(document.createElement('track'), {
-		kind: 'subtitles',
-		src: '/%s/subtitles.vtt?v=%d',
-		srclang: 'en',
-		label: 'English',
-		default: true,
-	}));
+  video.appendChild(
+    Object.assign(document.createElement("track"), {
+      kind: "subtitles",
+      src: "/%s/subtitles.vtt?v=%d",
+      srclang: "en",
+      label: "English",
+      default: true,
+    }),
+  );
 });
 hls.on(Hls.Events.ERROR, (event, data) => {
-	console.log('HLS Error:', data);
+  console.log("HLS Error:", data);
 });
-player.on('languagechange', () => {
-	setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+player.on("languagechange", () => {
+  setTimeout(() => (hls.subtitleTrack = player.currentTrack), 50);
 });
 `
+
+type List struct {
+	URL string
+	Dir string
+}
